af5a46174c6e2f5433b99e01315ed797a839d7ff
Leanne Stuive <lstuive@gmail.com>
2017-05-06 17:08:37 -0400

    Add server side code.

--- server/README.txt
+++ server/README.txt
@@ -4,5 +4,6 @@ DO ONCE
 
 DO ALWAYS
 3. start the database: mongod --
-4. seed the database: 
+4. seed the database:  mongo
+    load(“[yourpath]/seedscript.js”); 
 5. start the server: node index.js
---
+++ server/seedscript.js
@@ -0,0 +1,11 @@
+db.issues.insert([{
+     latitude : 43.471882,
+     longitude : -80.543972,
+     text : "Icy!",
+   }, 
+   {
+     latitude : 43.471173,
+     longitude : -80.550120,
+     text : "Broken button!"
+   }, ]
+);
